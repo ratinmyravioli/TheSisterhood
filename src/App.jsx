0cfcb7e207710bb7989af4dd469862ce9dc61059
ratinmyravioli <erikapoirier@ufl.edu>
2026-02-21 21:57:36 -0500

    Moved DB call into Forum.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import Chatbot from './chatbot';
-import { createClient } from '@supabase/supabase-js';
 
 import { Routes, Route, Link } from "react-router-dom";
 
@@ -11,27 +10,10 @@ import Map from "./pages/Map";
 import About from "./pages/About";
 import Forum from "./pages/Forum";
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
 
 function App() {
 
-  const [post, setPost] = useState([])
-
-  // EXAMPLE QUERY: this block of text just retrieves stuff from the database, does not show it.
-  useEffect(() => {
-    const fetchPost = async () => {
-      const {data, error } = await supabase
-      .from('forum_posts')
-      .select('id, title, body') // select all columns from database
-
-      if (error) {
-        console.log('Error fetching data:', error)
-      } else {
-        setPost(data)
-      }
-    }
-    fetchPost()
-  }, [])
 
   return (
     <>
@@ -58,16 +40,7 @@ function App() {
       </Routes>
 
 
-      {/* For displaying forum database */}
-      <div style={{padding: '2rem'}}> 
-        <h1>Forum</h1>
-        {post.map((item) => (
-          <div key = {item.id} style={{border: '1px solid #ccc', marginBottom: '1rem', padding: '0.5rem'}}>
-            <h2>{item.title}</h2>
-            <p>{item.body}</p>
-          </div>
-        ))}
-      </div>
+      
     </div>  
       
     <Chatbot />

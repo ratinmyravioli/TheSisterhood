ce6c633288868e05144021b9965a8e6245095013
Rishika <rishikajoshi@yahoo.com>
2026-02-21 19:05:07 -0500

    changed bot model

--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -14,7 +14,7 @@ export default function Chatbot() {
     const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
+        model: "gemini-2.5-flash-lite",
         contents: input,
       });
     
